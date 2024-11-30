ba06a71ef678713a2e655ef4d14556b82aa2f38b
abdulhadi <113355986+abdulhadi2001@users.noreply.github.com>
2024-11-30 12:45:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri="mongodb+srv://gulam01:Password@cluster0.oufqm3h.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 const connectWithDb = ()=>{
 
     mongoose.connect(process.env.DB_URI,{
